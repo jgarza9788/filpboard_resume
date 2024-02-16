be37a72bf8eddade1aedcb154eb2cbc7322a835a
Justin Garza <jgarza9788@gmail.com>
2024-02-15 22:18:02 -0500

    update matrix style

--- docs/script_v3.js
+++ docs/script_v3.js
@@ -2,124 +2,54 @@
 const splitFlapClasses = document.getElementsByClassName('splitflap');
 var chars = null;
 
-const speed_ms = 0;
-// const chance = 0.95;
+// const speed_ms = 0;
+var speed_ms = 0;
+var char_load = 10;
+var increment_char_load = 1;
+var max_char_load = 250;
 
-function getRandomString(length){
-
-  var result = ''
-  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!8182510647$%^&*()-_+=[]{}:<>?/| ';
-//   var characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюяΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩαβγδεζηθικλμνξοπρστυφχψω.,!?@#$%^&*()-_=+[]{};:|/<>`~€£¥₹©®™'
-//   var characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюяΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩαβγδεζηθικλμνξοπρστυφχψω.,!?@#$%^&*()-_=+[]{};:|/`~€£¥₹©®™'
-
-  for ( var i = 0; i < length; i++ ) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
- }
- return result;
+function endAnimation(){
+	chars = document.getElementsByTagName('char');
+	for (c of chars) {
+		c.innerText = c.getAttribute("char");
+		c.classList.add("char-done");
+	}
 }
 
-/*
-function nextCharacter(s){
-    return s.replace(/([a-zA-Z0-9])[^a-zA-Z]*$/, 
-		function(a){
 
-				if ( !isNaN(parseInt(a))  )
-				{
-					return ((parseInt(a) + 1)%10).toString();
-				}
+const body = document.querySelector('body');
+body.addEventListener("dblclick", (event) => {
 
-				if (a == 'z')
-				{
-					return 'A';
-				}
-				else if (a == 'Z')
-				{
-					return '0';
-				}
-				else if (a == '9')
-				{
-					return ' ';
-				}
+	console.log('double click start');
+
+	endAnimation();
+
+	console.log('end');
+});
 
 
-				var c= a.charCodeAt(0);
-				switch(c){
-					case 90: return 'A';
-					case 122: return 'a';
-					default: return String.fromCharCode(++c);
-				}
-			}
-	);
+var mylatesttap;
+function doubletap() {
+
+	var now = new Date().getTime();
+	var timesince = now - mylatesttap;
+	if((timesince < 600) && (timesince > 0))
+	{
+		endAnimation(); 
+	}
+
+	mylatesttap = new Date().getTime();
+
 }
-*/
+
+
 
 
 function nextCharacter(num){
-	var characters = ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789#@%*[]{}_-.’';
+	var characters = ' █▓▒░abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789#@%*[]{}_-.’ ';
 	// console.log(num%characters.length)
 	return characters[num%(characters.length)];
 }
-
-/*
-function nextCharacter(s,d){
-	var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!8182510647$%^&*()_+-=|<>?/;:[]{} 📞📧';
-	// console.log(num%characters.length)
-	// return characters[num%(characters.length)];
-
-	for (var i = 0; i < characters.length ;i++)
-	{
-		if (s == characters[i])
-		{
-			return characters[i+1];
-		}
-	}
-	return d;
-
-	// return characters[num%characters.length];
-}
-*/
-
-
-// for (var i = 0; i < 124;i++)
-// {
-// 	console.log(nextCharacter(i));
-// }
-
-
-/*
-console.log(nextCharacter(0))
-console.log(nextCharacter(10))
-console.log(nextCharacter(20))
-console.log(nextCharacter(40))
-console.log(nextCharacter(80))
-console.log(nextCharacter(160))
-console.log(nextCharacter(94))
-console.log(nextCharacter(95))
-console.log(nextCharacter(96))
-*/
-
-//test
-/*
-console.log(nextCharacter('A'));
-console.log(nextCharacter('a'));
-console.log(nextCharacter('Z'));
-console.log(nextCharacter('z'));
-console.log(nextCharacter('0'));
-console.log(nextCharacter('9'));
-console.log(nextCharacter('|'));
-*/
-
-/*
-function setup(){
-	for (sfc of splitFlapClasses) {
-
-		var text = sfc.getAttribute("text");
-		sfc.innerText = getRandomString(text.length);
-
-	}
-}
-setup();
-*/
 
 function setup(){
 	for (sfc of splitFlapClasses) {
@@ -134,8 +64,6 @@ function setup(){
 
 			sfc.appendChild(char);
 
-			// sfc.setAttribute("num",0);
-			// sfc.innerText = "A".repeat(text.length);
 		}
 		
 	}
@@ -144,19 +72,13 @@ setup();
 
 // console.log(chars);
 
-function replaceAt(str, index, replacement) {
-	// console.log(replacement);
-	if (index >= str.length) return str; // Index out of bounds
-	return str.substr(0, index) + replacement + str.substr(index + 1);
-}
 
 function transitionText(){
 	// console.log("transitionText");
 	chars = document.getElementsByTagName('char');
 	
-
 	var result = true;
-	var count = 0;
+	var char_count = 0;
 	for (c of chars) {
 
 		var char = c.getAttribute("char");
@@ -171,14 +93,14 @@ function transitionText(){
 		}
 
 		result = false;
-		count += 1;
-
-		if (count > 50)
+		
+		char_count += 1
+		if (char_count > char_load)
 		{
 			break;
 		}
 
-		if (num < 75)
+		if (num < 80)
 		{
 			c.innerText = nextCharacter(num);
 		}
@@ -191,7 +113,10 @@ function transitionText(){
 
 	} 
 	
+	char_load += increment_char_load;
+	char_load = Math.min(char_load,max_char_load);
 
+	console.log(char_load);
 	return result;
 }
 // transitionText();
@@ -204,8 +129,15 @@ function runUntilTrue() {
 	if (result) {
 		console.log("Function returned true, stopping.");
 	} else {
-		console.log("Function returned false, running again after 0.5 seconds.");
-		setTimeout(runUntilTrue, speed_ms); // Wait for 500 milliseconds before trying again
+		console.log("Function returned false, running again after 0.0 seconds.");
+		
+		// if (speed_ms > 0)
+		// {
+		// 	speed_ms -= 2;
+		// }
+
+		// speed_ms -= 2;
+		setTimeout(runUntilTrue, speed_ms); // Wait for 0 milliseconds before trying again
 	}
 }
 runUntilTrue();
